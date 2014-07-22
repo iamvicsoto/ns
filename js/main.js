d007c9e78b63f5c5f5dc28583bb0c19ff1d4ef3e
Vic Soto <vsbarba@gmail.com>
2014-07-22 15:10:35 +0800

    final

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
++function ($) { 'use strict'; 
+
+  $(document).ready(function () {
+  
+    $('[data-toggle-zoomThumb]').on('click', function() {
+      var $targetZoomCont = $(this).attr('data-toggle-zoomThumb');
+      $($targetZoomCont).attr('src', $(this).find('img').attr('src'));
+    });
+  
+  });
+               
+}(window.jQuery);
+
